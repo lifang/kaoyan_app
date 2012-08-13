03d7da8a5d8eef4bf442f000d8f4ead269e97ac2
qianjun <er6788@126.com>
2012-08-13 14:59:14 +0800

    qianjun

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaoyanApp::Application.config.secret_token = 'a448b86f323ca57014e261810e2c37134955dd16c3c00597397f1a2424a5d6b2c7c8dc2476f59120a8d4b08b7f08a3f5a9b8370affb68d76e32688bf3f6298f8'
