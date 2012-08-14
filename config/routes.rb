fac38a84fac70e9d7c6cf632d2bae1125d52c0b4
qianjun <er6788@126.com>
2012-08-14 10:26:16 +0800

    qianjun

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ KaoyanApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pretests#index'
 
   # See how all your routes lay out with "rake routes"
 
