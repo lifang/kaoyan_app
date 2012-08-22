428c1eee5a7defc2011029cfb953f2b3de5558f5
qianjun <er6788@126.com>
2012-08-22 16:54:38 +0800

    qianjun

--- app/controllers/pretests_controller.rb
+++ app/controllers/pretests_controller.rb
@@ -79,45 +79,5 @@ class PretestsController < ApplicationController
     end
   end
 
-  def baidu_connect
-    @api_key = LoginHelper::BAIDU_API_KEY
-    @baidu_callback =LoginHelper::BAIDU_CALLBACK
-    if params[:bd_user] && params[:bd_user]!="0"
-      @user=User.find_by_code_id_and_code_type(params[:bd_user],"baidu")
-      if @user
-        ActionLog.login_log(@user.id)
-        cookies[:user_id]=@user.id
-        cookies[:user_name]=@user.username
-        cookies.delete(:user_role)
-        user_order(Category::TYPE[:GRADUATE], cookies[:user_id].to_i)
-        redirect_to "/?category_id=#{Category::TYPE[:GRADUATE]}"
-        return false
-      end
-    end
-
-  end
-
-  def respond_baidu
-    http_route="/oauth/2.0/token?grant_type=authorization_code&code=#{params["code"]}&client_id=#{LoginHelper::BAIDU_API_KEY}
-    &client_secret=#{LoginHelper::BAIDU_SERECT_KEY}&redirect_uri=#{LoginHelper::BAIDU_CALLBACK}"
-    access_token=create_get_http(LoginHelper::BAIDU_API,http_route)
-    token_route="/rest/2.0/passport/users/getLoggedInUser?access_token=#{access_token["access_token"]}"
-    ret_user = create_get_http(LoginHelper::BAIDU_API,token_route)
-    @user=User.find_by_code_id_and_code_type("#{ret_user["uid"]}","baidu")
-    if @user.nil?
-      @user=User.create(:code_id=>ret_user["uid"],:code_type=>'baidu',:name=>ret_user["uname"],
-        :username=>ret_user["uname"], :access_token=>access_token, :end_time=>Time.now+access_token["expires_in"].to_i.seconds, :from => User::U_FROM[:APP ])
-    else
-      ActionLog.login_log(@user.id)
-      if @user.access_token.nil? || @user.access_token=="" || @user.access_token!=access_token
-        @user.update_attributes(:access_token=>access_token,:end_time=>Time.now+access_token["expires_in"].to_i.seconds)
-      end
-    end
-    cookies[:user_id]=@user.id
-    cookies[:user_name]=@user.username
-    cookies.delete(:user_role)
-    user_order(Category::TYPE[:GRADUATE], cookies[:user_id].to_i)
-    render :inline=>"<script type='text/javascript'>window.parent.location.href='/?category=#{Category::TYPE[:GRADUATE]}'</script>"
-  end
   
 end
